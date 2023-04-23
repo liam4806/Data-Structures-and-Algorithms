1f3f695e97f24a37e51642779cbfafad8952af17
LiamYoun <95008167+liam4806@users.noreply.github.com>
2023-04-23 13:40:20 +0900

    Create Digitgenerator.py

---
+++ Others/Digit Generator/Digitgenerator.py
@@ -0,0 +1,14 @@
+m=int(input())
+
+
+for n in range(1,m):
+    t=0
+    for i in range(len(str(n))):
+        t=t+int(str(n)[i])
+    x=n+t
+    if x==m:
+        print(n)
+        exit()
+    
+
+print(0)
